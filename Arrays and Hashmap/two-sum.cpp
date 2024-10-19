509995a37b02abff6b6208aa422c5a9689ab7437
souhardya <souhardya@MTXs-MacBook-Pro.local>
2024-10-19 00:19:03 -0400

    two sum done

--- Arrays and Hashmap/two-sum.cpp
+++ Arrays and Hashmap/two-sum.cpp
@@ -11,31 +11,31 @@ class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
         unordered_map<int,int> umap;
-        vector<int> res;
         for(int i=0;i<nums.size();i++){
-            if(umap.find(target - nums[i])!=umap.end()){
-                res.push_back(umap[target-nums[i]]);
-                res.push_back(i);
-                break;
-            }
+            if(umap.find(target - nums[i])!=umap.end())
+                return {umap[target-nums[i]],i};
             umap[nums[i]]=i;
         }
-        return res;
+        return {};
     }
 };
 
 int main(){
     vector<int> nums;
-    nums.push_back(2);
-    nums.push_back(7);
     nums.push_back(11);
-    nums.push_back(15);
+    nums.push_back(7);
+    nums.push_back(10);
+    nums.push_back(2);
 
     int target = 9;
 
     Solution s;
+
     cout<<"[";
-    for(int res : s.twoSum(nums,target))
-        cout << res <<",";
+    for(int i = 0; i<s.twoSum(nums,target).size(); i++){
+        cout << s.twoSum(nums,target)[i];
+        if(i < 1) 
+            cout<<",";
+    }
     cout<<"]";
 }
