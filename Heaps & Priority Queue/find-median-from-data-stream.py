3e4b82a4e7614709a723228fb5bc85c33b434e34
techchefsouho99 <souhardyaadhikary86942@gmail.com>
2025-07-18 05:06:53 -0400

    comments added

--- Heaps & Priority Queue/find-median-from-data-stream.py
+++ Heaps & Priority Queue/find-median-from-data-stream.py
@@ -2,6 +2,9 @@ import heapq
 class MedianFinder(object):
 
     def __init__(self):
+
+        # two heaps, large, small, minheap, maxheap
+        # heaps should be equal size  
         self.small, self.large = [] , []
 
     def addNum(self, num):
@@ -11,10 +14,12 @@ class MedianFinder(object):
         """
         heapq.heappush(self.small, -1 * num)
 
+        # make sure every num small is <= every num in large
         if(self.small and self.large and ((-1 * self.small[0]) > self.large[0])):
             val = -1 * heapq.heappop(self.small)
             heapq.heappush(self.large, val)
 
+        # uneven size
         if len(self.small) > len(self.large) + 1:
             val = -1 * heapq.heappop(self.small)
             heapq.heappush(self.large, val)
